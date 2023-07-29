a03dd1196276016ecc9db172fadb44d590560649
Piyapol Permpongpaiboon <piyapol.pppb@gmail.com>
2023-07-29 22:28:14 +0700

    feat(network): create private and public subnets

--- 01-network/config.ts
+++ 01-network/config.ts
@@ -1,17 +1,45 @@
 import * as pulumi from '@pulumi/pulumi'
 
 /**
+ * Represents a tag for a resource.
+ * @interface SubnetTag
+ * @property {string} key - The key of the tag.
+ * @property {string} value - The value of the tag.
+ */
+interface Tag {
+  key: string
+  value: string
+}
+
+/**
+ * Represents the configuration for a subnet.
+ * @interface SubnetConfig
+ * @property {string[]} cidrBlocks - An array of CIDR blocks for the subnet.
+ * @property {SubnetTag[]} [tags] - Optional array of tags associated with the subnet.
+ */
+interface SubnetConfig {
+  cidrBlocks: string[]
+  tags?: Tag[]
+}
+
+/**
  * Represents the configuration for a network.
  * @interface NetworkConfig
  * @property {string} name - Name to be used as identifier.
  * @property {string} cidrBlock - The CIDR block for the VPC.
+ * @property {string[]} availabilityZones - An array of availability zones for the subnets.
+ * @property {SubnetConfig} publicSubnet - The configuration for the public subnets.
+ * @property {SubnetConfig} privateSubnet - The configuration for the private subnets.
  */
 interface NetworkConfig {
   name: string
   cidrBlock: string
+  availabilityZones: string[]
+  publicSubnet: SubnetConfig
+  privateSubnet: SubnetConfig
 }
 
 const pulumiConfig = new pulumi.Config()
 const networkConfig = pulumiConfig.requireObject<NetworkConfig>('network')
 
-export { NetworkConfig, networkConfig }
+export { networkConfig, NetworkConfig, SubnetConfig }
--- 01-network/network.ts
+++ 01-network/network.ts
@@ -1,12 +1,74 @@
 import * as aws_native from '@pulumi/aws-native'
+import * as pulumi from '@pulumi/pulumi'
 
-import { NetworkConfig } from './config'
+import { NetworkConfig, SubnetConfig } from './config'
 
 /**
  * Represents the output of the network creation.
  */
 interface Network {
   vpc: aws_native.ec2.VPC
+  privateSubnets: aws_native.ec2.Subnet[]
+  publicSubnets: aws_native.ec2.Subnet[]
+}
+
+/**
+ * Represents the arguments required to create subnets in multiple availability zones within a VPC.
+ * @interface SubnetsArgs
+ * @property {string[]} availabilityZones - An array of availability zone names where the subnets will be created.
+ * @property {Boundary} boundary - The boundary configuration for the subnets.
+ * @property {string} name - Name to be used as identifier
+ * @property {SubnetConfig} subnetConfig - The configuration for the subnets.
+ * @property {string | pulumi.Input<string>} vpcId - The ID of the VPC in which the subnets will be created.
+ */
+interface SubnetsArgs {
+  availabilityZones: string[]
+  boundary: Boundary
+  name: string
+  subnetConfig: SubnetConfig
+  vpcId: string | pulumi.Input<string>
+}
+
+/**
+ * Enumeration representing the boundary type for creating subnets.
+ * @enum {string}
+ * @property {string} PRIVATE - Indicates a private boundary type.
+ * @property {string} PUBLIC - Indicates a public boundary type.
+ */
+enum Boundary {
+  PRIVATE = 'private',
+  PUBLIC = 'public',
+}
+
+/**
+ * Creates subnets in the VPC.
+ * @function
+ * @param {SubnetsArgs} subnetsArgs - The arguments required to create the subnets.
+ * @returns {aws_native.ec2.Subnet[]} - An array of AWS Native Subnet objects representing the created subnets.
+ */
+function createSubnets(subnetsArgs: SubnetsArgs): aws_native.ec2.Subnet[] {
+  const { availabilityZones, boundary, name, subnetConfig, vpcId } = subnetsArgs
+  const { cidrBlocks, tags = [] } = subnetConfig
+
+  const createdSubnets = cidrBlocks.map((cidrBlock, index) => {
+    const availabilityZone = availabilityZones[index]
+    const subnetName = `${name}-${boundary}-subnet-${availabilityZone}`
+
+    return new aws_native.ec2.Subnet(subnetName, {
+      availabilityZone,
+      cidrBlock,
+      vpcId,
+      tags: [
+        ...tags,
+        {
+          key: 'Name',
+          value: subnetName,
+        },
+      ],
+    })
+  })
+
+  return createdSubnets
 }
 
 /**
@@ -15,7 +77,13 @@ interface Network {
  * @returns {NetworkOutput} The output of the network creation.
  */
 function create(networkConfig: NetworkConfig): Network {
-  const { name, cidrBlock } = networkConfig
+  const {
+    availabilityZones,
+    cidrBlock,
+    name,
+    privateSubnet: privateSubnetConfig,
+    publicSubnet: publicSubnetConfig,
+  } = networkConfig
 
   // Create a VPC
   const vpcName = `${name}-vpc`
@@ -23,8 +91,29 @@ function create(networkConfig: NetworkConfig): Network {
     cidrBlock,
     tags: [{ key: 'Name', value: vpcName }],
   })
+  const { vpcId } = createdVpc
+
+  // Create private subnets
+  const createdPrivateSubnets = createSubnets({
+    availabilityZones,
+    boundary: Boundary.PRIVATE,
+    name,
+    subnetConfig: privateSubnetConfig,
+    vpcId,
+  })
+
+  // Create public subnets
+  const createdPublicSubnets = createSubnets({
+    availabilityZones,
+    boundary: Boundary.PUBLIC,
+    name,
+    subnetConfig: publicSubnetConfig,
+    vpcId,
+  })
 
   return {
+    privateSubnets: createdPrivateSubnets,
+    publicSubnets: createdPublicSubnets,
     vpc: createdVpc,
   }
 }
