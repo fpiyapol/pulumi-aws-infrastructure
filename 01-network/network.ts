01d2f8378feb3af7fd3d8ec41baed93c5eb9ffe7
Piyapol Permpongpaiboon <piyapol.pppb@gmail.com>
2023-07-30 12:02:13 +0700

    feat(network): create private and public subnets

--- 01-network/network.ts
+++ 01-network/network.ts
@@ -1,3 +1,4 @@
+import * as aws_classic from '@pulumi/aws'
 import * as aws_native from '@pulumi/aws-native'
 import * as pulumi from '@pulumi/pulumi'
 
@@ -7,9 +8,18 @@ import { NetworkConfig, SubnetConfig } from './config'
  * Represents the output of the network creation.
  */
 interface Network {
-  vpc: aws_native.ec2.VPC
+  internetGateway: aws_classic.ec2.InternetGateway
+  natGateway: aws_native.ec2.NatGateway
+  natGatewayEip: aws_native.ec2.EIP
+  privateRoute: aws_classic.ec2.Route
+  privateRouteAssociations: aws_native.ec2.SubnetRouteTableAssociation[]
+  privateRouteTable: aws_native.ec2.RouteTable
   privateSubnets: aws_native.ec2.Subnet[]
+  publicRoute: aws_classic.ec2.Route
+  publicRouteAssociations: aws_native.ec2.SubnetRouteTableAssociation[]
+  publicRouteTable: aws_native.ec2.RouteTable
   publicSubnets: aws_native.ec2.Subnet[]
+  vpc: aws_native.ec2.VPC
 }
 
 /**
@@ -111,8 +121,86 @@ function create(networkConfig: NetworkConfig): Network {
     vpcId,
   })
 
+  // Create a NAT Gateway
+  const eipNatName = `${name}-eip-nat`
+  const createdEipNat = new aws_native.ec2.EIP(eipNatName, {
+    tags: [{ key: 'Name', value: eipNatName }],
+  })
+  const natName = `${name}-nat`
+  const createdNatGateway = new aws_native.ec2.NatGateway(natName, {
+    allocationId: createdEipNat.allocationId,
+    subnetId: createdPublicSubnets[0].id,
+    tags: [{ key: 'Name', value: natName }],
+  })
+
+  // Create an Internet Gateway (IGW)
+  // aws_native does not support creating an IGW with a specified vpcId.
+  const igwName = `${name}-igw`
+  const createdInternetGateway = new aws_classic.ec2.InternetGateway(igwName, {
+    vpcId,
+    tags: { Name: igwName },
+  })
+
+  // Create a public route table and its route
+  const publicRouteTableName = `${name}-public-rtb`
+  const createdPublicRouteTable = new aws_native.ec2.RouteTable(
+    publicRouteTableName,
+    { vpcId, tags: [{ key: 'Name', value: publicRouteTableName }] },
+  )
+  const createdPublicRoute = new aws_classic.ec2.Route(`${name}-public-route`, {
+    destinationCidrBlock: '0.0.0.0/0',
+    gatewayId: createdInternetGateway.id,
+    routeTableId: createdPublicRouteTable.id,
+  })
+  const createdPublicRouteTableAssociations = createdPublicSubnets.map(
+    (subnet, index) => {
+      return new aws_native.ec2.SubnetRouteTableAssociation(
+        `${name}-${Boundary.PUBLIC}-rtb-assc-${index}`,
+        {
+          subnetId: subnet.id,
+          routeTableId: createdPublicRouteTable.id,
+        },
+      )
+    },
+  )
+
+  // Create a private route table and its route
+  const privateRouteTableName = `${name}-private-rtb`
+  const createdPrivateRouteTable = new aws_native.ec2.RouteTable(
+    privateRouteTableName,
+    { vpcId, tags: [{ key: 'Name', value: privateRouteTableName }] },
+  )
+  const createdPrivateRoute = new aws_classic.ec2.Route(
+    `${name}-private-route`,
+    {
+      destinationCidrBlock: '0.0.0.0/0',
+      natGatewayId: createdNatGateway.id,
+      routeTableId: createdPrivateRouteTable.id,
+    },
+  )
+  const createdPrivateRouteTableAssociations = createdPrivateSubnets.map(
+    (subnet, index) => {
+      return new aws_native.ec2.SubnetRouteTableAssociation(
+        `${name}-${Boundary.PRIVATE}-rtb-assc-${index}`,
+        {
+          subnetId: subnet.id,
+          routeTableId: createdPrivateRouteTable.routeTableId,
+        },
+      )
+    },
+  )
+
   return {
+    internetGateway: createdInternetGateway,
+    natGateway: createdNatGateway,
+    natGatewayEip: createdEipNat,
+    privateRoute: createdPrivateRoute,
+    privateRouteAssociations: createdPrivateRouteTableAssociations,
+    privateRouteTable: createdPrivateRouteTable,
     privateSubnets: createdPrivateSubnets,
+    publicRoute: createdPublicRoute,
+    publicRouteAssociations: createdPublicRouteTableAssociations,
+    publicRouteTable: createdPublicRouteTable,
     publicSubnets: createdPublicSubnets,
     vpc: createdVpc,
   }
